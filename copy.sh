b22453420398e61428218f95f5165ccdff8626f8
Junyoung Clare Jang <jjc9310@gmail.com>
2018-10-20 20:38:46 -0400

    Make scripts safer

--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-docker cp *.py dsm-to-elevation-array:/home/dsm-to-elevation-array
+docker cp ./*.py dsm-to-elevation-array:/home/dsm-to-elevation-array
 docker cp dsm.tif dsm-to-elevation-array:/home/dsm-to-elevation-array
 
 set +e
--- full_step.sh
+++ full_step.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-docker cp *.py dsm-to-elevation-array:/home/dsm-to-elevation-array
+docker cp ./*.py dsm-to-elevation-array:/home/dsm-to-elevation-array
 docker cp dsm.tif dsm-to-elevation-array:/home/dsm-to-elevation-array
 docker start dsm-to-elevation-array
 docker exec dsm-to-elevation-array python main.py dsm.tif result.json 100
