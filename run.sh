d5f2b81201e4ed44fcaf81a015cef060938364ee
Junyoung Clare Jang <jjc9310@gmail.com>
2018-10-22 18:38:50 -0400

    Fix run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 set -e
 
 docker start dsm-to-elevation-array
-docker exec dsm-to-elevation-array python main.py dsm.tif result.json
+docker exec dsm-to-elevation-array python main.py dsm.tif result.json 100
 docker stop dsm-to-elevation-array
 
 set +e
